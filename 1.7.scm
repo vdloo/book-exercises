3b82437669e1aef6661535f213473916f463ac8d
Rick van de Loo <rickvandeloo@gmail.com>
2014-06-26 19:45:33 +0000

    formatting

--- 1.7.scm
+++ 1.7.scm
@@ -25,8 +25,8 @@
 
 ; The test isn't very effective for small numbers because 0.001 isn't a very significant threshold to test if the guess is good enough, the estimation
 ; of the square root gets more efficient as the number grows larger because the 0.001 will pose less and less of a relative difference.
-(sqrt 0.0009)	; 0.03			procedure returns .04030062264654547
-(sqrt 9) 			; 3 				procedure returns 3.00009155413138
+(sqrt 0.0009)	; 0.03      procedure returns .04030062264654547
+(sqrt 9) 			; 3         procedure returns 3.00009155413138
 (sqrt 900000) ; 948.683.. procedure returns 948.6832980505158
 
 ;The alternative implementation of good-enough where the difference of guess from one iteration to the next is compared based on a fraction of the guess.
@@ -38,6 +38,6 @@
 
 ; The test is more effective for small numbers but for large numbers it is less accurate. This is because as the numbers grow the absolute value of 0.001
 ; will be less than whatever 0.001 * guess is. So as long as the guess is larger than 1, this good-enough test will be less accurate than the previous one. 
-(sqrt 0.0009)	; 0.03			procedure returns .03002766742182557
-(sqrt 9) 			; 3 				procedure returns 3.00009155413138
+(sqrt 0.0009)	; 0.03      procedure returns .03002766742182557
+(sqrt 9) 			; 3         procedure returns 3.00009155413138
 (sqrt 900000) ; 948.683.. procedure returns 949.0206159647947

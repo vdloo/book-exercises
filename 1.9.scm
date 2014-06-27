be97659b9aeb9f349b1b8074885961324621f77e
Rick van de Loo <rickvandeloo@gmail.com>
2014-06-27 17:36:51 +0000

    1.9 fixed bug due to capturing the variable +

--- 1.9.scm
+++ 1.9.scm
@@ -5,39 +5,55 @@
   (- x 1)
 )
 
-; This procedure is recursive, the inc procedure is deferred every time the function calls itself
-; this results in (inc (+ (dec a b)) becoming (inc (inc (+ dec a b)) etc.
+; The first process is recursive, the inc procedure is deferred every time the function calls itself
+; this results in (inc (add (dec a b)) becoming (inc (inc (add dec a b)) etc.
 
-; (+ 4 5)
-; (inc (+ (dec 4) 5))
-; (inc (+ 3 5))
-; (inc (inc (+ (dec 3) 5)))
-; (inc (inc (+ 2 5)))
-; (inc (inc (inc (+ (dec 2) 5))))
-; (inc (inc (inc (+ 1 5))))
-; (inc (inc (inc (inc (+ (dec 1) 5)))))
-; (inc (inc (inc (inc (+ 0 5)))))
+; (add 4 5)
+; (inc (add (dec 4) 5))
+; (inc (add 3 5))
+; (inc (inc (add (dec 3) 5)))
+; (inc (inc (add 2 5)))
+; (inc (inc (inc (add (dec 2) 5))))
+; (inc (inc (inc (add 1 5))))
+; (inc (inc (inc (inc (add (dec 1) 5)))))
+; (inc (inc (inc (inc (add 0 5)))))
 ; (inc (inc (inc (inc 5))))
 ; (inc (inc (inc 6)))
 ; (inc (inc 7))
 ; (inc 8)
 ; 9
 
-(define (+ a b)
+(define (add a b)
   (display b)
   (if (= a 0)
       b
-      (inc (+ (dec a) b))
+      (inc (add (dec a) b))
   )
 )
-(+ 4 5)
+(add 4 5)
 
-; This procedure is iterative because all the state variables are passed into the next iteration,
+; The second process is iterative because all the state variables are passed into the next iteration,
 ; there is no need for the interpreter to keep track of what happens outside of the current iteration. 
-(define (+ a b)
+
+; (add 4 5)
+; (add (dec 4) (inc 5))
+; (add 3 6)
+; (add (dec 3) (inc 6))
+; (add 2 7)
+; (add (dec 2) (inc 7))
+; (add 1 8)
+; (add (dec 1) (inc 8))
+; (add 0 9)
+; 9
+;
+(define (add a b)
   (if (= a 0)
       b
-      (+ (dec a) (inc b))
+      (add (dec a) (inc b))
   )
 )
-(+ 4 5)
+(add 4 5)
+
+; Both procedures are recursive because they refer to themselves, however the difference is that the first
+; procedure shows an implementation of a recursive process and the second procedure displays an implementation 
+; of an iterative process (which makes it tail-recursive). 

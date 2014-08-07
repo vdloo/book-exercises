41d5148be5b24e437ca444ddeae7fe75e3328cda
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-07 16:11:52 +0000

    procedure to compute fib with log number of steps

--- 1.19.scm
+++ 1.19.scm
@@ -23,4 +23,30 @@
 ;
 ; Tp'q':  A <- B(Q^2 + 2PQ) + A(Q^2 + 2PQ) + A(P^2 + Q^2)
 ;         B <- B(P^2 + Q^2) + A(Q^2 + 2PQ)
-;
+
+(define (iseven n) (= (remainder n 2) 0))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((iseven count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* q q) (* 2 (* p q)))
+                   (/ count 2)
+         )
+        )
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)
+              )
+        )
+  )
+)
+(define (fib n)
+  (fib-iter 1 0 0 1 n)
+)
+
+(fib 50)

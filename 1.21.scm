bde7fb9fa776f6d913ac701455591534deb9f0b3
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-09 12:51:51 +0000

    results

--- 1.21.scm
+++ 1.21.scm
@@ -11,6 +11,6 @@
   (= (remainder b a) 0)
 )
 
-(smallest-divisor 199)
-(smallest-divisor 1999)
-(smallest-divisor 19999)
+(smallest-divisor 199)    ; 199
+(smallest-divisor 1999)   ; 1999
+(smallest-divisor 19999)  ; 7

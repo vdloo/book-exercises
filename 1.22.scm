ae853a68f5bd10495b94d0b00e28ca114359f3cc
Rick van de Loo <rickvandeloo@gmail.com>
2014-08-10 12:19:50 +0000

    timed-prime-test procedure from book

---
+++ 1.22.scm
@@ -0,0 +1,40 @@
+(define (divides? a b)
+  (= (remainder b a) 0)
+)
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))
+  )
+)
+
+(define (smallest-divisor n)
+  (find-divisor n 2)
+)
+
+(define (prime? n)
+  (= n (smallest-divisor n))
+)
+
+(define (time-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))
+  )
+)
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+)
+
+(time-prime-test 9)
+(time-prime-test 11)
+(time-prime-test 15485863)
+(time-prime-test 15485864)

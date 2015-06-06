b1cfad379635fa754a6aa3889cbfbc6b58718cde
Rick van de Loo <rickvandeloo@gmail.com>
2015-06-06 23:18:45 +1000

    exercise 2.15

---
+++ 2.15.scm
@@ -0,0 +1,5 @@
+; Both the formulas for parallel resistors are algebraically the same. But
+; because the R values are intervals this doesn't work out, dividing an
+; interval by itself does not result in 1 (it approaches it). Because of this,
+; par2 is more accurate because by not repeating variables that represent
+; uncertain numbers a tighter error bound is produced.

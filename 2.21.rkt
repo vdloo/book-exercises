afbed1dcfdddf739b36c3c8e72bc6e2abe089170
Rick van de Loo <rickvandeloo@gmail.com>
2015-09-30 18:06:09 +0200

    exercise 2.21

---
+++ 2.21.rkt
@@ -0,0 +1,17 @@
+#!/usr/bin/env racket
+#lang racket
+
+(define (square x)
+  (* x x))
+
+(define (square-list items)
+  (if (null? items)
+    '()
+    (cons (square (car items)) (square-list (cdr items)))))
+
+(square-list (list 1 2 3 4))
+
+(define (square-list2 items)
+  (map square items))
+
+(square-list2 (list 1 2 3 4))

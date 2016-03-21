b6057cf61be1adb41c24110b0cd2a662cd39a8de
Rick van de Loo <rickvandeloo@gmail.com>
2016-03-21 07:57:55 +0100

    exercise 2.16

---
+++ 2.16.scm
@@ -0,0 +1,4 @@
+; Explain, in general, why equivalent algebraic expressions may lead to different
+; answers. Can you devise an interval-arithmetic package that does not have this shortcoming, or is this ; task impossible? (
+
+; Because if the same interval is present multiple times in the process it can result in unwanted expansions when each occurrence is taken independently. You could stop that from happening by rewriting the function to eliminate double variables but not all functions are capable of that.

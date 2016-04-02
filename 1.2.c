52aebf6d9727b40d93cc358b99420ea92a717c58
Rick van de Loo <rickvandeloo@gmail.com>
2016-04-02 12:51:17 +0200

    exercise 1.2

---
+++ 1.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Printing alert (bell) character \a.\n");
+    printf("Printing backspace character \b.\n");
+    printf("Printing formfeed character \f.\n");
+    printf("Printing carriage return character \r.\n");
+    printf("Printing vertical tab character \v.\n");
+    printf("Printing backslash character \\.\n");
+    printf("Printing question mark character \?.\n");
+    printf("Printing single quote character \'.\n");
+    printf("Printing double quote character \".\n");
+    printf("Printing octal number %o.\n", '\012');
+    printf("Printing hex number %X.\n", '\x12');
+    printf("Done!\n");
+}

cce0eaa9a5329ef774bedb86f23dde2a33d99ef9
Rick van de Loo <rickvandeloo@gmail.com>
2016-04-05 07:49:19 +0200

    exercise 1.5

---
+++ 1.5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* print reverse Fahrenheit-Celsius table */
+
+void print_heading()
+{
+    char *f = "fahr";
+    char *c = "celcius";
+    printf("%3s %3s\n", f, c);
+}
+
+void print_table() 
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = upper;
+    while (fahr >= lower) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+	fahr = fahr - step;
+    }
+}
+
+main()
+{
+    print_heading();
+    print_table();
+}

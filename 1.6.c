b810f386320783623531bd6030b55645d231fa8c
Rick van de Loo <rickvandeloo@gmail.com>
2016-04-06 08:02:07 +0200

    exercise 1.6

---
+++ 1.6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+    int i = getchar() != EOF;
+    if (i == 1) {
+	/* it does not equal EOF if you send input over stdin*/
+        printf("getchar() does not equal EOF\n");
+    }
+    else {
+	/* it does equal EOF if you ctrl + d */
+        printf("getchar() equals EOF\n");
+    }
+}

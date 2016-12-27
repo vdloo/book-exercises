a7e80fc9a74d57e06beea2322cb4e57d102fa205
Rick van de Loo <rickvandeloo@gmail.com>
2016-12-27 18:33:13 +0100

    forward checking for parenths, braces and curlies
    can detect missing trailing parethesis, braces and curly brackets but
    not yet missing opening characters.

--- 1.24.c
+++ 1.24.c
@@ -67,11 +67,12 @@ void enter_state(int new_state)
     enterline[nest_level] = lines + 1;
 }
 
-void exit_state()
+void exit_state(int state_to_exit)
 {
     /* Drop a state from the stack */
     extern int state[MAXNESTED];
     extern int nest_level;
+	/* todo: throw error when state_to_exit not in stack */
     state[nest_level] = '\0';
     nest_level--;
 }
@@ -111,6 +112,87 @@ void shift_read_character()
     prevchar = c;
 }
 
+void handle_re_entrant_state()
+{
+	/* From the a re-entrant state we can enter the comment state, 
+	 * the single quote state and the double quote state. We can also
+	 * enter the parenthesis, brackets and braces states. Those last 
+	 * three can be nested */
+	if (detect_entering_comment()) {
+		enter_state(COMMENT);
+	} else if (detect_state_change('\'')) {
+		enter_state(SINGLEQ);
+	} else if (detect_state_change('"')) {
+		enter_state(DOUBLEQ);
+	} else if (detect_state_change('(')) {
+		enter_state(PARENTH);
+	} else if (detect_state_change(')')) {
+		exit_state(PARENTH);
+	} else if (detect_state_change('[')) {
+		enter_state(BRACKET);
+	} else if (detect_state_change(']')) {
+		exit_state(BRACKET);
+	} else if (detect_state_change('{')) {
+		enter_state(CURLIES);
+	} else if (detect_state_change('}')) {
+		exit_state(CURLIES);
+	}
+}
+
+void handle_comment()
+{
+	/* From the comment state we can only exit back into the normal 
+	 * state. We can not have come from the comment single quote 
+	 * state and we could also not have come from the double quote state 
+	 * since those both do not allow entering the comment state. */
+	if (detect_leaving_comment()) {
+		exit_state(COMMENT);
+		if (get_current_state() == SINGLEQ || get_current_state() == DOUBLEQ) {
+			fprintf(
+				stderr, 
+				"Could not parse comment at line %d\n",
+				lines
+			);
+			exit(150);
+		}
+	}
+}
+
+void handle_q(char delimiter, int state, int check, char msg[], int code)
+{
+	if (detect_state_change(delimiter)) {
+		exit_state(state);
+		if (get_current_state() == check) {
+			fprintf(
+					stderr, 
+					"%s at line %d\n",
+					msg, lines
+				   );
+			exit(code);
+		}
+	}
+}
+
+void handle_singleq()
+{
+	/* From the single quote state we can exit back into the 
+	 * normal state. The previous state should only be the normal 
+	 * state since the char constant of double quote should not 
+	 * change the state. And because character constants are by 
+	 * definition only one char long, we should also never enter 
+	 * the comment state.*/
+	handle_q('\'', SINGLEQ, DOUBLEQ, "Could not parse single quotes", 151);
+}
+
+void handle_doubleq()
+{
+	/* From the double quote state we can only revert back into the 
+	 * normal state since we can't enter a comment state from within
+	 * a multi-line comment. Additionally, we can not have come from 
+	 * the single quote state. */
+	handle_q('"', DOUBLEQ, SINGLEQ, "Could not parse double quotes", 152);
+}
+
 void initialize_state_machine()
 {
     while ((c = getchar()) != EOF) {
@@ -118,69 +200,17 @@ void initialize_state_machine()
             lines++;
         }
         switch(get_current_state()) {
-            case NORMAL:
-                /* From the normal state we can enter the comment state, 
-                 * the single quote state and the double quote state. */
-                if (detect_entering_comment()) {
-                    enter_state(COMMENT);
-                } else if (detect_state_change('\'')) {
-                    enter_state(SINGLEQ);
-                } else if (detect_state_change('"')) {
-                    enter_state(DOUBLEQ);
-                }
+            case NORMAL: case PARENTH: case BRACKET: case CURLIES:
+				handle_re_entrant_state();
                 break;
             case COMMENT:
-                /* From the comment state we can only exit back into the normal 
-                 * state. We can not have come from the comment single quote 
-                 * state and we could also not have come from the double quote state 
-                 * since those both do not allow entering the comment state. */
-                if (detect_leaving_comment()) {
-                    exit_state();
-                    if (get_current_state() != NORMAL) {
-                        fprintf(
-                            stderr, 
-                            "Could not parse comment at line %d\n",
-                            lines
-                        );
-                        exit(150);
-                    }
-                }
+				handle_comment();
                 break;
             case SINGLEQ:
-                /* From the single quote state we can exit back into the 
-                 * normal state. The previous state should only be the normal 
-                 * state since the char constant of double quote should not 
-                 * change the state. And because character constants are by 
-                 * definition only one char long, we should also never enter 
-                 * the comment state.*/
-                if (detect_state_change('\'')) {
-                    exit_state();
-                    if (get_current_state() != NORMAL) {
-                        fprintf(
-                            stderr, 
-                            "Could not parse single quotes at line %d\n",
-                            lines
-                        );
-                        exit(151);
-                    }
-                }
+				handle_singleq();
                 break;
             case DOUBLEQ:
-                /* From the double quote state we can only revert back into the 
-                 * normal state since we can't enter a comment state from within
-                 * a multi-line comment. Additionally, we can not have come from 
-                 * the single quote state. */
-                if (detect_state_change('"')) {
-                    exit_state();
-                    if (get_current_state() != NORMAL) {
-                        fprintf(
-                            stderr, 
-                            "Could not parse double quotes at %d\n",
-                            lines
-                        );
-                        exit(152);
-                    }
-                }
+				handle_doubleq();
                 break;
         }
         shift_read_character();
@@ -199,8 +229,10 @@ void trace_unbalanced()
             names[get_current_state()],
             get_line_of_state(nest_level)
         );
-        exit(153);
-    }
+        exit(156);
+    } else {
+		printf("It all looks ok!\n");
+	}
 }
 
 int main(void)

69c3e957e59549248326a6008bf5217c64ecceb5
Rick van de Loo <rickvandeloo@gmail.com>
2017-01-18 22:06:45 +0100

    tabs are spaces in 2.6.c

--- 2.6.c
+++ 2.6.c
@@ -100,204 +100,204 @@
 
 unsigned int get_least_three_significant_bits(unsigned int bits)
 {
-	return bits & ~(~0 << 3);
+    return bits & ~(~0 << 3);
 }
 
 unsigned int move_bits_up_n_slots(unsigned int bits, int n)
 {
-	assert(n > 0);
-	return bits << n;
+    assert(n > 0);
+    return bits << n;
 }
 
 unsigned int zero_out_n_to_least_significant(unsigned int bits, int n)
 {
-	assert(n > 0);
-	return bits & (~0 << n);
+    assert(n > 0);
+    return bits & (~0 << n);
 }
 
 unsigned int zero_out_except_n_to_m(unsigned int bits, int n, int m)
 {
-	assert(m < n);
-	assert(n > 0);
-	assert(m >= 0);
-	return bits & ((~0 << m) & (~(~0 << (n + 1))));
+    assert(m < n);
+    assert(n > 0);
+    assert(m >= 0);
+    return bits & ((~0 << m) & (~(~0 << (n + 1))));
 }
 
 unsigned int zero_out_n_to_m(unsigned int bits, int n, int m)
 {
-	assert(m < n);
-	assert(n > 0);
-	assert(m >= 0);
-	return bits & ~((~0 << m) & (~(~0 << (n + 1))));
+    assert(m < n);
+    assert(n > 0);
+    assert(m >= 0);
+    return bits & ~((~0 << m) & (~(~0 << (n + 1))));
 }
 
 void test_zero_out_n_to_m()
 {
-	unsigned int bits;
-	unsigned int ret;
+    unsigned int bits;
+    unsigned int ret;
 
-	/* Test zeroing out bits 1 and 0 in 7 */
-	bits = 07;
-	ret = zero_out_n_to_m(bits, 1, 0);
-	printf("Zeroing out bits 1 and 0 in %d gives %d\n", bits, ret);
-	assert(ret == 4);
+    /* Test zeroing out bits 1 and 0 in 7 */
+    bits = 07;
+    ret = zero_out_n_to_m(bits, 1, 0);
+    printf("Zeroing out bits 1 and 0 in %d gives %d\n", bits, ret);
+    assert(ret == 4);
 
-	/* Test zeroing out bits 2, 1 and 0 in 7 */
-	bits = 07;
-	ret = zero_out_n_to_m(bits, 2, 0);
-	printf("Zeroing out bits 2, 1 and 0 in %d gives %d\n", bits, ret);
-	assert(ret == 0);
+    /* Test zeroing out bits 2, 1 and 0 in 7 */
+    bits = 07;
+    ret = zero_out_n_to_m(bits, 2, 0);
+    printf("Zeroing out bits 2, 1 and 0 in %d gives %d\n", bits, ret);
+    assert(ret == 0);
 
-	/* Test zeroing out bits 3 and 2 in 7*/
-	bits = 07;
-	ret = zero_out_n_to_m(bits, 3, 2);
-	printf("Zeroing out bits 3 and 2 in %d gives %d\n", bits, ret);
-	assert(ret == 3);
+    /* Test zeroing out bits 3 and 2 in 7*/
+    bits = 07;
+    ret = zero_out_n_to_m(bits, 3, 2);
+    printf("Zeroing out bits 3 and 2 in %d gives %d\n", bits, ret);
+    assert(ret == 3);
 
-	/* Test zeroing out bits 3 and 2 in 15*/
-	bits = 017;
-	ret = zero_out_n_to_m(bits, 3, 2);
-	printf("Zeroing out bits 3 and 2 in %d gives %d\n", bits, ret);
-	assert(ret == 3);
+    /* Test zeroing out bits 3 and 2 in 15*/
+    bits = 017;
+    ret = zero_out_n_to_m(bits, 3, 2);
+    printf("Zeroing out bits 3 and 2 in %d gives %d\n", bits, ret);
+    assert(ret == 3);
 
-	/* Test zeroing out bits bits 5 to 1 in 7*/
-	bits = 0377;
-	ret = zero_out_n_to_m(bits, 5, 1);
-	/* 
-	 * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 
-	 * |128| 64| 32| 16| 8 | 4 | 2 | 1 |
-	 * | 1 | 1 | 0 | 0 | 0 | 0 | 0 | 1 |
-	 *   193 = 128 + 64 + 1 */
-	printf("Zeroing out bits 5, 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 193);
+    /* Test zeroing out bits bits 5 to 1 in 7*/
+    bits = 0377;
+    ret = zero_out_n_to_m(bits, 5, 1);
+    /* 
+     * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 
+     * |128| 64| 32| 16| 8 | 4 | 2 | 1 |
+     * | 1 | 1 | 0 | 0 | 0 | 0 | 0 | 1 |
+     *   193 = 128 + 64 + 1 */
+    printf("Zeroing out bits 5, 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 193);
 }
 
 void test_zero_out_except_n_to_m()
 {
-	unsigned int bits;
-	unsigned int ret;
+    unsigned int bits;
+    unsigned int ret;
 
-	/* Test zeroing out all except bits 1 and 0 in 7 */
-	bits = 07;
-	ret = zero_out_except_n_to_m(bits, 1, 0);
-	printf("Keeping bits 1 and 0 in %d gives %d\n", bits, ret);
-	assert(ret == 3);
+    /* Test zeroing out all except bits 1 and 0 in 7 */
+    bits = 07;
+    ret = zero_out_except_n_to_m(bits, 1, 0);
+    printf("Keeping bits 1 and 0 in %d gives %d\n", bits, ret);
+    assert(ret == 3);
 
-	/* Test zeroing out all except bits 2, 1 and 0 in 7 */
-	bits = 07;
-	ret = zero_out_except_n_to_m(bits, 2, 0);
-	printf("Keeping bits 2, 1 and 0 in %d gives %d\n", bits, ret);
-	assert(ret == 7);
+    /* Test zeroing out all except bits 2, 1 and 0 in 7 */
+    bits = 07;
+    ret = zero_out_except_n_to_m(bits, 2, 0);
+    printf("Keeping bits 2, 1 and 0 in %d gives %d\n", bits, ret);
+    assert(ret == 7);
 
-	/* Test zeroing out all except bits 3 and 2 in 7*/
-	bits = 07;
-	ret = zero_out_except_n_to_m(bits, 3, 2);
-	printf("Keeping bits 3 and 2 in %d gives %d\n", bits, ret);
-	assert(ret == 4);
+    /* Test zeroing out all except bits 3 and 2 in 7*/
+    bits = 07;
+    ret = zero_out_except_n_to_m(bits, 3, 2);
+    printf("Keeping bits 3 and 2 in %d gives %d\n", bits, ret);
+    assert(ret == 4);
 
-	/* Test zeroing out all except bits 3 and 2 in 15*/
-	bits = 017;
-	ret = zero_out_except_n_to_m(bits, 3, 2);
-	printf("Keeping bits 3 and 2 in %d gives %d\n", bits, ret);
-	assert(ret == 12);
+    /* Test zeroing out all except bits 3 and 2 in 15*/
+    bits = 017;
+    ret = zero_out_except_n_to_m(bits, 3, 2);
+    printf("Keeping bits 3 and 2 in %d gives %d\n", bits, ret);
+    assert(ret == 12);
 
-	/* Test zeroing out all except bits 5 to 1 in 7*/
-	bits = 0777777;
-	ret = zero_out_except_n_to_m(bits, 5, 1);
-	/* 
-	 * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 
-	 * |128| 64| 32| 16| 8 | 4 | 2 | 1 |
-	 * | 0 | 0 | 1 | 1 | 1 | 1 | 1 | 0 |
-	 *   62 = 32 + 16 + 8 + 4 + 2 */
-	printf("Keeping bits 5, 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 62);
+    /* Test zeroing out all except bits 5 to 1 in 7*/
+    bits = 0777777;
+    ret = zero_out_except_n_to_m(bits, 5, 1);
+    /* 
+     * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 
+     * |128| 64| 32| 16| 8 | 4 | 2 | 1 |
+     * | 0 | 0 | 1 | 1 | 1 | 1 | 1 | 0 |
+     *   62 = 32 + 16 + 8 + 4 + 2 */
+    printf("Keeping bits 5, 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 62);
 }
 
 void test_zero_out_n_to_least_significant()
 {
-	unsigned int bits;
-	unsigned int ret;
+    unsigned int bits;
+    unsigned int ret;
 
-	/* Test zeroing out bits 2 and 1 in 7*/
-	bits = 7;
-	ret = zero_out_n_to_least_significant(bits, 2);
-	printf("Zeroing out bits 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 4);
+    /* Test zeroing out bits 2 and 1 in 7*/
+    bits = 7;
+    ret = zero_out_n_to_least_significant(bits, 2);
+    printf("Zeroing out bits 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 4);
 
-	/* Test zeroing out bits 3, 2 and 1 in 7*/
-	bits = 7;
-	ret = zero_out_n_to_least_significant(bits, 3);
-	printf("Zeroing out bits 3, 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 0);
+    /* Test zeroing out bits 3, 2 and 1 in 7*/
+    bits = 7;
+    ret = zero_out_n_to_least_significant(bits, 3);
+    printf("Zeroing out bits 3, 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 0);
 
-	/* Test zeroing out bits 3, 2 and 1 in 15*/
-	bits = 017;
-	ret = zero_out_n_to_least_significant(bits, 3);
-	printf("Zeroing out bits 3, 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 8);
+    /* Test zeroing out bits 3, 2 and 1 in 15*/
+    bits = 017;
+    ret = zero_out_n_to_least_significant(bits, 3);
+    printf("Zeroing out bits 3, 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 8);
 
-	/* Test zeroing out bits 4, 3, 2 and 1 in 15*/
-	bits = 017;
-	ret = zero_out_n_to_least_significant(bits, 4);
-	printf("Zeroing out bits 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
-	assert(ret == 0);
+    /* Test zeroing out bits 4, 3, 2 and 1 in 15*/
+    bits = 017;
+    ret = zero_out_n_to_least_significant(bits, 4);
+    printf("Zeroing out bits 4, 3, 2 and 1 in %d gives %d\n", bits, ret);
+    assert(ret == 0);
 
 }
 
 void test_move_bits_up_n_slots()
 {
-	unsigned int bits;
-	unsigned int ret;
+    unsigned int bits;
+    unsigned int ret;
 
-	/* Test moving bits up once multiplies the number by 2 */
-	bits = 7;
-	ret = move_bits_up_n_slots(bits, 1);
-	printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
-	assert(ret == 14);
+    /* Test moving bits up once multiplies the number by 2 */
+    bits = 7;
+    ret = move_bits_up_n_slots(bits, 1);
+    printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
+    assert(ret == 14);
 
-	/* Test moving bits up twice multiplies the number by 4 */
-	bits = 7;
-	ret = move_bits_up_n_slots(bits, 2);
-	printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
-	assert(ret == 28);
+    /* Test moving bits up twice multiplies the number by 4 */
+    bits = 7;
+    ret = move_bits_up_n_slots(bits, 2);
+    printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
+    assert(ret == 28);
 
-	/* Test moving bits up three times multiplies the number by 8 */
-	bits = 7;
-	ret = move_bits_up_n_slots(bits, 3);
-	printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
-	assert(ret == 56);
+    /* Test moving bits up three times multiplies the number by 8 */
+    bits = 7;
+    ret = move_bits_up_n_slots(bits, 3);
+    printf("Moving the bits in %d up by 1 gives %d\n", bits, ret);
+    assert(ret == 56);
 }
 
 void test_get_at_least_three_significant_bits()
 {
-	unsigned int bits;
-	unsigned int ret;
+    unsigned int bits;
+    unsigned int ret;
 
-	/* Test the original value is not mutated */
-	bits = 1234;
-	get_least_three_significant_bits(bits);
-	assert(bits == 1234);
+    /* Test the original value is not mutated */
+    bits = 1234;
+    get_least_three_significant_bits(bits);
+    assert(bits == 1234);
 
-	/* Test up until 7 the least significant bits add up to the int */
-	for (bits = 0; bits < 7; bits++) {
-		ret = get_least_three_significant_bits(bits);
-		printf("The least three significant bits of %d count to %d\n", bits, ret);
-		assert(ret == bits);
-	}
+    /* Test up until 7 the least significant bits add up to the int */
+    for (bits = 0; bits < 7; bits++) {
+        ret = get_least_three_significant_bits(bits);
+        printf("The least three significant bits of %d count to %d\n", bits, ret);
+        assert(ret == bits);
+    }
 
-	/* Test at 8 the least significant bits are all off */
-	bits = 8;
-	ret = get_least_three_significant_bits(bits);
-	printf("The least three significant bits of %d count to %d\n", bits, ret);
-	assert(ret == 0);
+    /* Test at 8 the least significant bits are all off */
+    bits = 8;
+    ret = get_least_three_significant_bits(bits);
+    printf("The least three significant bits of %d count to %d\n", bits, ret);
+    assert(ret == 0);
 
-	/* Test that anything larger than 8 is cut off from the result, 
-	 * those are the bits higher than the first three slots */
-	for (bits = 9; bits < 16; bits++) {
-		ret = get_least_three_significant_bits(bits);
-		printf("The least three significant bits of %d count to %d\n", bits, ret);
-		assert(ret == bits - 8);
-	}
+    /* Test that anything larger than 8 is cut off from the result, 
+     * those are the bits higher than the first three slots */
+    for (bits = 9; bits < 16; bits++) {
+        ret = get_least_three_significant_bits(bits);
+        printf("The least three significant bits of %d count to %d\n", bits, ret);
+        assert(ret == bits - 8);
+    }
 }
 
 unsigned setbits(unsigned int x, int p, int n, unsigned int y) {
@@ -306,44 +306,44 @@ unsigned setbits(unsigned int x, int p, int n, unsigned int y) {
 
 void test_setbits()
 {
-	unsigned int x, y, ret;
-	int p, n;
+    unsigned int x, y, ret;
+    int p, n;
 
     x = 170;
-	y = 238;
+    y = 238;
     p = 4;
     n = 3;
 
-	ret = setbits(x, p, n, y);
+    ret = setbits(x, p, n, y);
     printf(
         "The result of copying the last %d bits of "\
-		"%d to position %d of %d is %d\n",
+        "%d to position %d of %d is %d\n",
         n, y, p, x, ret
     );
     assert(ret == 186);
 
     x = 213;
-	y = 121;
+    y = 121;
     p = 4;
     n = 4;
 
-	ret = setbits(x, p, n, y);
+    ret = setbits(x, p, n, y);
     printf(
         "The result of copying the last %d bits of "\
-		"%d to position %d of %d is %d\n",
+        "%d to position %d of %d is %d\n",
         n, y, p, x, ret
     );
     assert(ret == 211);
 
     x = 5732;
-	y = 9823;
+    y = 9823;
     p = 6;
     n = 3;
 
-	ret = setbits(x, p, n, y);
+    ret = setbits(x, p, n, y);
     printf(
         "The result of copying the last %d bits of "\
-		"%d to position %d of %d is %d\n",
+        "%d to position %d of %d is %d\n",
         n, y, p, x, ret
     );
     assert(ret == 5748);
@@ -351,12 +351,12 @@ void test_setbits()
 
 int main(void)
 {
-	test_get_at_least_three_significant_bits();
-	test_move_bits_up_n_slots();
-	test_zero_out_n_to_least_significant();
-	test_zero_out_except_n_to_m();
-	test_zero_out_n_to_m();
-	test_setbits();
+    test_get_at_least_three_significant_bits();
+    test_move_bits_up_n_slots();
+    test_zero_out_n_to_least_significant();
+    test_zero_out_except_n_to_m();
+    test_zero_out_n_to_m();
+    test_setbits();
     return 0;
 }
 

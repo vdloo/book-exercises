6a2e6f13af906f55e7992ffa2f0e3ffe6dd11808
Rick van de Loo <rickvandeloo@gmail.com>
2018-01-24 07:38:56 +0100

    exercise 2.9

---
+++ 2.9.hs
@@ -0,0 +1,10 @@
+--- Exercise 2.9
+--- Show, using the truth table from Exercise 2.2, that
+--- (P ⊻ Q) ⊻ Q is equivalent to P
+---
+--- (P ⊻ Q) ⊻ Q ⇔ P
+---  t f t  t t t t
+---  t t f  t f t t
+---  f t t  f t t f
+---  f f f  f f t f
+

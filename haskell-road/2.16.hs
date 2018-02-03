08840348e7c8c939671aef15e13601c3a4cc01d3
Rick van de Loo <rickvandeloo@gmail.com>
2018-02-03 14:22:41 +0100

    exercise 2.16

---
+++ haskell-road/2.16.hs
@@ -0,0 +1,9 @@
+--- Exercise 2.16
+--- Procude useful denials for every sentence of Exercise 2.31
+---
+--- 1. The equation x^2 + 1 = 0 has no solution
+--- 2. There exists a large natural number
+--- 3. The number 13 is not prime
+--- 4. The number n is not prime
+--- 5. There is only a finite amount of primes
+
